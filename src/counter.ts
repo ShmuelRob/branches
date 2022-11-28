f8e73d36e6669363e84ac8e9f6cef3370ec42c08
Shmuel Robinov <shmuel.robinov@gmail.com>
2022-11-28 12:15:06 +0200

    change3

--- src/counter.ts
+++ src/counter.ts
@@ -3,9 +3,12 @@ export function setupCounter(element: HTMLButtonElement) {
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
+    
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0);
+
+  
 
   
   // some changes
