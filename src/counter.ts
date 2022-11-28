6123f116dd4c7c85a07ac74143d5c0b1f0051b11
Shmuel Robinov <shmuel.robinov@gmail.com>
2022-11-28 11:04:18 +0200

    try2

--- src/counter.ts
+++ src/counter.ts
@@ -7,13 +7,7 @@ export function setupCounter(element: HTMLButtonElement) {
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0);
 
-  // q: how do I open a new branch on git?
-  // a: git checkout -b <branch-name>
-
-  // q: how to solve differential equation?
-  // a: use ode45
-
-  // q: how to solve integral?
-  // a: use quad
+  
+  // some changes
 
 }
